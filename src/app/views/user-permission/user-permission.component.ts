8fee9dd2e730db27b57181449dd94271abeb8988
Mohammadreza Taherabadi <m.taher@pishgaman.net>
2022-01-17 15:28:31 +0330

    portmanUi

--- src/app/views/user-permission/user-permission.component.ts
+++ src/app/views/user-permission/user-permission.component.ts
@@ -23,12 +23,16 @@ export class UserPermissionComponent implements OnInit {
 
   
   dslam_permissions:Permission[];
-  selectedPermissions: Permission;
+  command_permissions:Permission[];
+  selected_dslamPermissions: Permission;
+  selected_commandPermissions: Permission;
   user_permissions : Permission[];
   pagination_config;
   users_permissions = [];
   add_permission_profile : boolean = false;
-  view_user_permission_profile: boolean = false;
+  view_user_dslam_permission_profile: boolean = false;
+  view_user_command_permission_profile: boolean = false;
+
   paginate(event) {
     this.pagination_config.currentPage = event.page + 1;
     this.pagination_config.itemsPerPage = event.rows;
@@ -42,15 +46,23 @@ export class UserPermissionComponent implements OnInit {
     });
   }
 
-  get_user_permission_profile(permission_profile_id){
+  get_user_permission_profile(permission_profile_id, model_type){
     this.usrSrv.get_user_permission_profile(permission_profile_id).then(res => {
-      if(res.result){
-        this.dslam_permissions = res.result;
-        this.selectedPermissions = res.result;
-        this.user_permissions = res.result;
+      if(res.result){     
+      if(model_type == 1){
+        this.dslam_permissions = res.result.filter((element) => element.model_type === 'dslam');
+        this.selected_dslamPermissions = res.result.filter((element) => element.model_type === 'dslam');
+        this.user_permissions = res.result.filter((element) => element.model_type === 'dslam');
+        this.view_user_dslam_permission_profile = true;
+      }
+      else{
+        this.command_permissions = res.result.filter((element) => element.model_type === 'command');
+        this.selected_commandPermissions = res.result.filter((element) => element.model_type === 'command');
+        this.user_permissions = res.result.filter((element) => element.model_type === 'command');
+        this.view_user_command_permission_profile = true;
 
       }
-      this.view_user_permission_profile = true;
+    }
     });
   }
 
