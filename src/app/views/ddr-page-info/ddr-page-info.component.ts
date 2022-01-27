cc9dad6761c9bc0f71579eefd857f8116ef07fe1
Mohammadreza Taherabadi <m.taher@pishgaman.net>
2022-01-27 17:17:15 +0330

    portmanUi

--- src/app/views/ddr-page-info/ddr-page-info.component.ts
+++ src/app/views/ddr-page-info/ddr-page-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../../../services/contact.service';
-
+import * as fileSaver from 'file-saver';
+import * as xlsx from "xlsx"; 
 @Component({
   selector: 'app-ddr-page-info',
   templateUrl: './ddr-page-info.component.html',
@@ -18,6 +19,7 @@ export class DdrPageInfoComponent implements OnInit {
    pagination_config;
    farzanegan_data = [];
    farzanegan_data_total;
+
    get_ddr_info(username, currentPage, itemsPerPage){
     this.contSrv.get_ddr_info(username, currentPage, itemsPerPage).then(res=>{
       this.farzanegan_data = res.results;
@@ -38,6 +40,31 @@ export class DdrPageInfoComponent implements OnInit {
     this.pagination_config.itemsPerPage = event.rows;
     this.get_ddr_info('',this.pagination_config.currentPage, this.pagination_config.itemsPerPage);    
   }
+
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.farzanegan_data);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "Data");
+    });
+}
+
+
+saveAsExcelFile(buffer: any, fileName: string): void {
+  import("file-saver").then(FileSaver => {
+    let EXCEL_TYPE =
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+    let EXCEL_EXTENSION = ".xlsx";
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(
+      data,
+      fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+    );
+  });
+}
 
   ngOnInit(): void {
     this.get_ddr_info_total('Iran-sepanta');
